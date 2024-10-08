a7eac7229b128e3129e2aa121a2bd2cfe8305f98
jbrijs <joebrijs@gmail.com>
2024-10-07 23:37:44 -0600

    k frequent elements solution

---
+++ leetcode/arrays/topKFrequent.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        map = {}
+        buckets = [[] for _ in range(len(nums) + 1)]
+        res = []
+
+        for num in nums:
+            map[num] = map.get(num, 0) + 1
+
+        for key, value in map.items():
+            buckets[value].append(key)
+
+        for i in range(len(buckets) - 1, 0, -1):
+            for val in buckets[i]:
+                res.append(val)
+                if len(res) == k:
+                    return res
+
+      
